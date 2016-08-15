8274ade0839ad507dcbd355c8573d531d4aef90f
adonis-dio <adonias.tsouza@gmail.com>
2016-08-15 17:00:14 -0300

    better close solution and animations

--- app/gallery.component.ts
+++ app/gallery.component.ts
@@ -6,36 +6,49 @@ import { Http, HTTP_PROVIDERS } from 'angular2/http';
     selector: 'gallery',
     providers: [GalleryService],
     styles: [`
-    .img-holder { overflow: hidden; float: left; width: 200px } 
+    .img-thumb { overflow: hidden; float: left; width: 200px; cursor: pointer; transition: 0.3s; } 
+    .img-thumb:hover { opacity: 0.7; } 
     .selected { opacity: 0.5 }
     .imodal { width:100%; height:100%; left: 0; top: 0; padding-top: 10%; overflow: auto; position: fixed; z-index: 999; background-color: rgb(0, 0, 0); background-color: rgba(0, 0, 0, 0.9) }
     .imodal-content { margin: auto; display: block; width: 80%; max-width: 900px; }
-    .caption { margin: 10px; width: 80%; max-width: 900px; text-align: center; color: #ccc; padding: 10px 0; height: 150px; }
+    .caption { text-align: center; color: #ccc; }
+    .close-btn { cursor:pointer; position: absolute; right: 0; top: 0; padding: 10px; color: #ccc; }
+    .close-btn:hover { color: #fff; }
+    .imodal-content, .caption { -webkit-animation-name: zoom; -webkit-animation-duration: 0.6s; animation-name: zoom; animation-duration: 0.6s;}
+    @-webkit-keyframes zoom { from {-webkit-transform:scale(0)} to {-webkit-transform:scale(1)}}
+    @keyframes zoom { from {transform:scale(0)} to {transform:scale(1)}}
     `],
     template: `
-    <div *ngFor="#image of images | async" (click)="select(image)">
+    <div *ngFor="#image of images | async" (click)="select(image)">        
+        <div class="text-center img-thumb"><img src="{{ image.url }}" height="200" [class.selected]="isSelected == image" alt="{{ image.alt }}"></div>
         
         <div class="text-center imodal" *ngIf="isSelected == image">
+            <span class="close-btn" (click)="deselect()"><h1>&times;</h1></span>
             <img src="{{ image.url }}" class="imodal-content">
             <span class="caption">{{ image.alt }}</span>
         </div>
-        <div class="text-center img-holder"><img src="{{ image.url }}" height="200" [class.selected]="isSelected == image" alt="{{ image.alt }}"></div>
+
     </div>
     `
 })
 
 export class GalleryComponent {
     images: any;
-    isSelected;
+    isSelected = false;
+    close = false;
 
     constructor(private _galleryService: GalleryService) {
         this.images = _galleryService.getFriends();
     }
 
     select(image){
-        if(this.isSelected != image)
+        if(this.isSelected != image && this.close == false)
             this.isSelected = image;
-        else
+        else if(this.isSelected == image && this.close == true)
             this.isSelected = false;
+            this.close = false;
+    }
+    deselect(){
+        this.close = true;
     }
 } 
